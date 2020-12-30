07dfe2fd884c70260fdc820d4250664ddb5f8de7
Mariano Zicavo <marianozicavo@users.noreply.github.com>
2020-12-30 16:42:24 -0300

    Update index.tsx

--- wbooks/src/app/index.tsx
+++ wbooks/src/app/index.tsx
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 
 import { store } from '../redux/store';
 
-function App() {
+function App()   {
   return (
     <>
       <StatusBar barStyle="dark-content" />
